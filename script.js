320e818ee89cf10b6791b4e0ee90435adbb17b39
Fernando <beratechsolutions@gmail.com>
2023-05-13 18:58:25 -0300

    inserindo javascript do projeto

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.menu');
+conts NavMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    
+})
